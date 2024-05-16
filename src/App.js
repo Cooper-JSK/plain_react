9dc1756dab6b761738230db47ac5e6c9ab2a784c
CryoWarlocK <karunarathna_js_e23@engug.ruh.ac.lk>
2024-05-17 01:05:20 +0530

    Done the tutorial

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ imdbID: "tt0831387"
 const App = () => {
 
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
 
     const searchMovies = async (title) => {
        const response = await fetch (`${API_URL}&s=${title}`);   
@@ -41,29 +42,28 @@ const App = () => {
             <input 
             placeholder='Search for movies'
             type = 'text'
-            value = "superman"
-            onChange= {() => {}}
+            value = {searchTerm}
+            onChange= {(e) => setSearchTerm(e.target.value)}
             />
             <img 
             src = {SearchIcon}
-            alt = 'search'/>
-            onClick = {() => {}}
-
+            alt = 'search'
+            onClick = {() => searchMovies(searchTerm)}
+            />
         </div>
 
         {
             movies?.length > 0 ?
-            (<div className = "container">
-                {movies.map((movie) => (
-                    <MovieCard movie = {movie} />
-                ))}
-
-            </div>
+            (
+                <div className = "container">
+                    {movies.map((movie) => (
+                    <   MovieCard movie = {movie} />
+                    ))}
+                </div>
             ) : (
-            <div className = "empty">
-                <h2>No Movies Found</h2>
-
-            </div>)
+                <div className = "empty">
+                    <h2>No Movies Found</h2>
+                </div>)
         }
 
     </div>
